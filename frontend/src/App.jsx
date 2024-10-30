19e6633da24ce6d7e35e9a9fc31dcc32d455e10f
PAVAN28GIT <pavankmjp@gmail.com>
2024-10-30 21:35:37 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-4xl text-red-400'>
+      sdfsdfs
+    </div>
+  )
+}
+
+export default App
