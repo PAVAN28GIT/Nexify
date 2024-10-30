c4c57b057ea7f3159732b555f211ab1a68cbc7af
PAVAN28GIT <pavankmjp@gmail.com>
2024-10-30 22:53:34 +0530

    pages and components created + routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+import LandingPage from './pages/landing/LandingPage'
+import SignInPage from './pages/signin/SignInPage'
+import SignUpPage from './pages/signup/SignUpPage'
+import NoPage from './pages/NoPage/NoPage'
+
+
+import HomePage from './pages/home/HomePage'
+import Home from './pages/home/Home'
+import CommunityPage from './pages/Community/CommunityPage'
+import Community from './pages/Community/Community'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-4xl text-red-400'>
-      sdfsdfs
+    <div>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
+      
+        <Route path="/user/:userId" element={<HomePage />}>
+          <Route index element={<Home />} />
+          <Route path="communities" element={<CommunityPage />} />
+          <Route path="community/:communityId" element={<Community />} />
+          <Route path="profile/:profileId" element={<ProfilePage />} />
+        </Route>
+        <Route path='*' element={<NoPage />} />
+      </Routes>
     </div>
   )
 }
